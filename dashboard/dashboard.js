dfc446d24c197bf7af5d308185c1e3e4478681a2
raysr <raysamram@protonmail.com>
2018-10-25 23:13:05 +0100

    adding dashobard interface and some js scripts

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,5 +1,10 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.AutoInit();
- 
+    var collapses=document.querySelectorAll(".collapsible-body");
+    var titles=document.querySelectorAll(".collapsible-header")
+    for(i in collapses)
+    {
+        collapses[i].innerHTML=titles[i].textContent;
+    }
   });
